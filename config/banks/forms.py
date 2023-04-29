c73f842170bd695bb6cf18b3d21fee4f369c9bf0
Evgeny <95162969+Evgeny122@users.noreply.github.com>
2023-04-29 11:42:57 +0300

    Delete config directory

--- config/banks/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import BankModel
-
-
-class ClientForm(forms.Form):
-    price = forms.IntegerField()
-    contribution = forms.IntegerField()
-    years = forms.IntegerField() 
--- config/banks/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class BankModel(models.Model):
-    name_banks = models.CharField(max_length=20)
-    mortgage_rate = models.FloatField(default=0.0)
-    payment = models.IntegerField(default=0)
